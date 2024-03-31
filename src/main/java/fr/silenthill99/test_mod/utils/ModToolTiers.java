9ce1d162f6c83b456b3a398563cb56c4f35676d1
silenthill99 <florian.graziani@sfr.fr>
2024-04-01 00:28:46 +0200

    Remise en forme

--- src/main/java/fr/silenthill99/test_mod/utils/ModToolTiers.java
+++ src/main/java/fr/silenthill99/test_mod/utils/ModToolTiers.java
@@ -12,8 +12,7 @@ import net.minecraftforge.common.TierSortingRegistry;
 
 import java.util.List;
 
-public class ModToolTiers
-{
+public class ModToolTiers {
     public static final Tier SAPPHIRE = TierSortingRegistry.registerTier(
             new ForgeTier(5, 1500, 5f, 4f, 25, ModTags.Blocks.NEEDS_SAPPHIRE_TOOLS, () -> Ingredient.of(ModItems.SAPPHIRE.get()))
             ,new ResourceLocation(Main.MODID, "sapphire"), List.of(Tiers.NETHERITE), List.of());
