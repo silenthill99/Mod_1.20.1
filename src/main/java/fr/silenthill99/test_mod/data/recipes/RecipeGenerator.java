aba655ab44d708daea7c233b90bd99835bd99e8b
BuildTools <unconfigured@null.spigotmc.org>
2024-05-14 11:14:51 +0200

    Rajout des recettes des outils

--- src/main/java/fr/silenthill99/test_mod/data/recipes/RecipeGenerator.java
+++ src/main/java/fr/silenthill99/test_mod/data/recipes/RecipeGenerator.java
@@ -7,7 +7,9 @@ import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Consumer;
 
@@ -19,7 +21,7 @@ public class RecipeGenerator extends RecipeProvider
     }
 
     @Override
-    protected void buildRecipes(Consumer<FinishedRecipe> consumer) {
+    protected void buildRecipes(@NotNull Consumer<FinishedRecipe> consumer) {
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.SAPPHIRE.get(), 9)
                 .requires(ModBlocks.SAPPHIRE_BLOCK.get())
                 .unlockedBy("unlock", InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.SAPPHIRE_BLOCK.get()))
@@ -51,6 +53,56 @@ public class RecipeGenerator extends RecipeProvider
                 .pattern("00")
                 .define('0', ModBlocks.SAPPHIRE_BLOCK.get())
                 .unlockedBy("unlock", InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.SAPPHIRE_BLOCK.get()))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.SAPPHIRE_PICKAXE.get())
+                .pattern("000")
+                .pattern(" 1 ")
+                .pattern(" 1 ")
+                .define('0', ModItems.SAPPHIRE.get())
+                .define('1', Items.STICK)
+                .unlockedBy("unlock", InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.SAPPHIRE.get(),
+                        Items.STICK))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.SAPPHIRE_AXE.get())
+                .pattern("00")
+                .pattern("01")
+                .pattern(" 1")
+                .define('0', ModItems.SAPPHIRE.get())
+                .define('1', Items.STICK)
+                .unlockedBy("unlock", InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.SAPPHIRE.get(),
+                        Items.STICK))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.SAPPHIRE_HOE.get())
+                .pattern("00")
+                .pattern(" 1")
+                .pattern(" 1")
+                .define('0', ModItems.SAPPHIRE.get())
+                .define('1', Items.STICK)
+                .unlockedBy("unlock", InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.SAPPHIRE.get(),
+                        Items.STICK))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.SAPPHIRE_SHOVEL.get())
+                .pattern("0")
+                .pattern("1")
+                .pattern("1")
+                .define('0', ModItems.SAPPHIRE.get())
+                .define('1', Items.STICK)
+                .unlockedBy("unlock", InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.SAPPHIRE.get(),
+                        Items.STICK))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.SAPPHIRE_SWORD.get())
+                .pattern("0")
+                .pattern("0")
+                .pattern("1")
+                .define('0', ModItems.SAPPHIRE.get())
+                .define('1', Items.STICK)
+                .unlockedBy("unlock", InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.SAPPHIRE.get(),
+                        Items.STICK))
                 .save(consumer);
     }
 }
