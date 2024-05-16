d3ed622801070d1ed52ee0cd1b4d68f05e164dc2
BuildTools <unconfigured@null.spigotmc.org>
2024-05-16 20:37:03 +0200

    Ajout de cultures

---
+++ src/main/java/fr/silenthill99/test_mod/custom/blocks/StrawberryCropBlock.java
@@ -0,0 +1,39 @@
+package fr.silenthill99.test_mod.custom.blocks;
+
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import org.jetbrains.annotations.NotNull;
+
+public class StrawberryCropBlock extends CropBlock {
+    public static final int MAX_AGE = 5;
+    public static final IntegerProperty AGE = BlockStateProperties.AGE_5;
+
+    public StrawberryCropBlock(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    protected @NotNull ItemLike getBaseSeedId() {
+        return super.getBaseSeedId();
+    }
+
+    @Override
+    protected @NotNull IntegerProperty getAgeProperty() {
+        return AGE;
+    }
+
+    @Override
+    public int getMaxAge() {
+        return MAX_AGE;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.@NotNull Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(AGE);
+    }
+}
